699d49735a5e3da2b6d417cb61955d3deba773fd
nav0727 <dosaninaveenreddy@gmail.com>
2022-09-21 07:19:27 +0000

    firstcommit

---
+++ src/components/ClickCounter/index.js
@@ -0,0 +1,30 @@
+/* eslint-disable react/style-prop-object */
+import {Component} from 'react'
+
+import './index.css'
+
+class ClickCounter extends Component {
+  state = {count: 0}
+
+  onClickMe = () => {
+    this.setState(prevState => ({count: prevState.count + 1}))
+  }
+
+  render() {
+    const {count} = this.state
+    return (
+      <div className="container">
+        <h1 className="heading">
+          The Button has been clicked <br />
+          <span className="count">{count}</span> times
+        </h1>
+        <p> Click the button to increase the count! </p>
+        <button type="button" className="btn" onClick={this.onClickMe}>
+          Click Me!
+        </button>
+      </div>
+    )
+  }
+}
+
+export default ClickCounter
